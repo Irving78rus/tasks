21506b0b0a8007caa5ef4ea674fd673c967d5ea0
Irving78rus <54670767+Irving78rus@users.noreply.github.com>
2020-09-03 23:35:25 +0300

    Update time.js

--- time.js
+++ time.js
@@ -1,13 +1,16 @@
-
-var number = [1, 2, 3, 4];
-console.log(number)
+var number = [9, 2, 4, 3];
 
 function largestTimeFromDigits(number) {
     var fall = "Такой массив на время не разделить"
+    var fall1 = "Такой массив на время не разделить1"
+    var fall2 = "Такой массив на время не разделить2"
     var num = 2;
     var num1 = 4;
+    var num11 = 9;
     var num2 = 5;
     var digit = -1;
+    var digit1 = -1;
+    var digit2 = -1;
     var final = [];
     console.log(number)
     while (digit == -1) {
@@ -20,62 +23,64 @@ function largestTimeFromDigits(number) {
             }
         }
         num--
-        if (num == 0) {
+        if (num == -1) {
             return fall
         }
     }
 
     var index = number.indexOf(digit);
     number.splice(index, 1);
-    digit = -1;
 
-    while (digit == -1) {
+    while (digit1 == -1) {
 
         for (var i = 0; i < number.length; i++) {
-            if (number[i] == num1) {
-                digit = number[i]
-                final.push(digit)
+            if ((number[i] == num1) && (digit == 2)) {
+                digit1 = number[i]
+                final.push(digit1)
+                console.log(final + ' final');
+                break;
+            }
+            else if ((number[i] == num11) && (digit == 1)) {
+                digit1 = number[i]
+                final.push(digit1)
+                console.log(final + ' final');
                 break;
             }
         }
-        num1--
-        if (num == 0) {
-            return fall
+        if (digit == 2) {
+            num1--
+        }
+        if (digit == 1) {
+            num11--
+        }
+        if ((num1 == -1) || (num11 == -1)) {
+            return fall1
         }
     }
-
-    var index = number.indexOf(digit);
+    var index = number.indexOf(digit1);
     number.splice(index, 1);
-    digit = -1;
-
     final.push(":")
 
-    while (digit == -1) {
+    while (digit2 == -1) {
 
         for (var i = 0; i < number.length; i++) {
             if (number[i] == num2) {
-                digit = number[i]
-                final.push(digit)
+                digit2 = number[i]
+                final.push(digit2)
                 break;
             }
         }
-        num2--
-        if (num == 0) {
-            return fall
+        --num2
+        if (num2 == -1) {
+            return fall2
         }
     }
-
-    var index = number.indexOf(digit);
+    var index = number.indexOf(digit2);
     number.splice(index, 1);
-    digit = -1;
+
     final.push(number[0])
-
-    console.log(final);
-
-    console.log(digit);
-    console.log(number);
-
+    var str = final.join('');
+    return str
 }
-
-var qwq = largestTimeFromDigits(number)
-console.log(qwq);
+var result = largestTimeFromDigits(number)
+console.log(result);
